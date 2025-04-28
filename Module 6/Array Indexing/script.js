07fb449bc3aad05f309e7e52b7891728b0351f83
Tendedpiano <jackroe749@gmail.com>
2025-04-28 18:14:55 +0100

    Delete Module 6 directory

--- Module 6/Array Indexing/script.js
+++
@@ -1,8 +0,0 @@
-const books = ["Lord of the flies",
-    "To kill a mockingbird", 
-    "House of Leaves",
-    "Horrible Histories", 
-    "The Great Gatsby"
-];
-
-console.log(books);
--- Module 6/Array Methods/script.js
+++
@@ -1,6 +0,0 @@
-const books = ["Lord of the flies",
-    "To kill a mockingbird", 
-    "House of Leaves",
-    "Horrible Histories", 
-    "The Great Gatsby"
-];

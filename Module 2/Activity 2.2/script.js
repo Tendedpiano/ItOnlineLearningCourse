70185d99cdbebc586f680f2c344e75b7455e5af4
Tendedpiano <jackroe749@gmail.com>
2025-04-28 18:14:17 +0100

    Delete Module 2 directory

--- Module 2/Activity 2.2/script.js
+++
@@ -1,6 +0,0 @@
-function changeHeadingText() {
-    // Targets the heading by its unique id
-    var headingElement = document.getElementById("myheading");
-    // Assign new text to the heading element
-    headingElement.textContent = "New Heading Text";
-}
